4278491d07bea6d779995d61c2a7f86aeb12aeff
enigmatic-agent-scully <amanda.elisabet.jordan@gmail.com>
2018-09-25 13:46:30 -0400

    main buttons work, click to animate works, add show button does not work

--- assets/javascript/javascript.js
+++ assets/javascript/javascript.js
@@ -1,15 +1,17 @@
 var topics = ["The X-Files", "Person of Interest", "Wynonna Earp", "Warehouse 13", "Buffy", "Wentworth",];
 var buttonDiv = $(".buttons");
 var i = 0;
+var results;
 
-$(Window).on("load", buildButtons());
+$(window).on("load", buildButtons());
 
 function buildButtons() {
     topics.forEach(function () {
-    var button = $(`<button class = "show" data-name = "${topics[i]}">${topics[i]}</button>`);
-    buttonDiv.append(button);
-    i++;
-} );}
+        var startingButton = $(`<button class = "show" data-name = "${topics[i]}">${topics[i]}</button>`);
+        buttonDiv.append(startingButton);
+        i++;
+    });
+}
 
 $(".show").click(function displayGif() {
     var show = $(this).attr("data-name");
@@ -18,50 +20,50 @@ $(".show").click(function displayGif() {
     $.ajax({
         url: queryURL,
         method: "GET"
-    }).then(function(response) {
+    }).then(function (response) {
         console.log(queryURL);
-        console.log(response);
+        results = response;
+        console.log(results);
         var newGifDiv = $("<div class='display'>");
         $(".gifs").empty();
         $(".gifs").append(newGifDiv);
-        for (var i = 0; i < response.data.length; i++) {
-            var rating = response.data[i].rating;
+        for (var i = 0; i < results.data.length; i++) {
+            var rating = results.data[i].rating;
             var gifDisplay = $(`
             <figure>
-            <img src="${response.data[i].images.fixed_height_still.url}" alt="${response.data[i].title}">
+            <img src="${results.data[i].images.fixed_height_still.url}" alt="${results.data[i].title}" data-still="${results.data[i].images.fixed_height_still.url}" data-animate="${results.data[i].images.fixed_height.url}" data-state="still">
             <figcaption>Rating: ${rating}</figcaption>
             </figure>
             `);
             newGifDiv.append(gifDisplay);
         }
-        
     }
     )
 });
 
-$("#add-button").on("click", function addButton(event) {
-    event.preventDefault();
+$("#add-button").on("click", function addButton() {
     var showName = $("#add-a-show").val();
     topics.push(showName);
     console.log(topics);
-    buttonDiv.empty();
-    for (var j = 0; j < topics.length; i++)
+    topics.forEach(function () {
         var button = $(`<button class = "show" data-name = "${topics[i]}">${topics[i]}</button>`);
         buttonDiv.append(button);
+        i++;
     }
-    );
+    )
+});
 
-$("figure").on("click", function animate(event) {
-    event.preventDefault();
-    var that = $(this);
-    console.log(that);
-    if (that.src == `"${response.data[i].images.fixed_height_still.url}"`) {
-        that.attr("src") `"${response.data[i].images.fixed_height.url}"`;
+$("div.gifs").on("click", "img", function animate() {
+    var state = $(this).attr("data-state");
+    if (state === "still") {
+        $(this).attr("src", $(this).attr("data-animate"));
+        $(this).attr("data-state", "animate");
     }
-    else if (that.src == `"${response.data[i].images.fixed_height.url}"`) {
-        that.src = `"${response.data[i].images.fixed_height_still.url}"`
+    else {
+        $(this).attr("src", $(this).attr("data-still"));
+        $(this).attr("data-state", "still");
     }
-})
+});
 
 
 
